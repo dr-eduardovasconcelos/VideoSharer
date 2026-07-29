5f3b15d4729c2c6ff93b454fa0d879cb14f9416f
dr-eduardovasconcelos <56181935+dr-eduardovasconcelos@users.noreply.github.com>
2026-07-29 12:26:15 -0300

    resolvendo problema do notfound

--- src/main/java/com/devcaotics/videoSharer/VideoSharerApplication.java
+++ src/main/java/com/devcaotics/videoSharer/VideoSharerApplication.java
@@ -3,7 +3,7 @@ package com.devcaotics.videoSharer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.devcaotics")
 public class VideoSharerApplication {
 
 	public static void main(String[] args) {
