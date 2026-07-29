dfe6162d1f80cfbb397ea9174b2b0354084060ba
dr-eduardovasconcelos <56181935+dr-eduardovasconcelos@users.noreply.github.com>
2026-07-29 17:53:18 -0300

    Change init method to return ResponseEntity

--- src/main/java/com/devcaotics/videoSharer/webSocket/Controller.java
+++ src/main/java/com/devcaotics/videoSharer/webSocket/Controller.java
@@ -4,15 +4,16 @@ import org.springframework.stereotype.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 @RequestMapping("/api/rest")
 @CrossOrigin("https://videosharer.onrender.com/")
 public class Controller{
   @GetMapping("/ids")
-  public String init(){ 
+  public ResponseEntity<?> init(){ 
     
-      return ServiceHolder.getIds();
+      return ResponseEntity.ok(ServiceHolder.getIds());
     
   }
 
