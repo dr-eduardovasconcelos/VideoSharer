33613d04a65e5b196d7561bcf2217d14396367df
dr-eduardovasconcelos <56181935+dr-eduardovasconcelos@users.noreply.github.com>
2026-07-29 17:56:51 -0300

    Update Controller.java

--- src/main/java/com/devcaotics/videoSharer/webSocket/Controller.java
+++ src/main/java/com/devcaotics/videoSharer/webSocket/Controller.java
@@ -1,6 +1,6 @@
 package com.devcaotics.videoSharer.webSocket;
 
-import org.springframework.stereotype.RestController;
+import  org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
